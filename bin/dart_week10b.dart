43ba5b055b422ea16fd62e74c06ca99e44f98f10
patipan-patisampita <patipan-patisampita@gmail.com>
2023-07-20 09:31:21 +0700

    update

---
+++ bin/dart_week10b.dart
@@ -0,0 +1,8 @@
+import 'package:dart_week10b/number.dart';
+
+void main() {
+  ternaryOpertor();
+  //numBer();
+}
+
+
---
+++ lib/number.dart
@@ -0,0 +1,20 @@
+void ternaryOpertor() {
+  int num1 = 10;
+  int num2 = 15;
+  String message = "";
+  message = (num1 > num2) ? 'The number is $num1' : 'The number is $num2';
+  print(message);
+}
+
+void numBer() {
+  int num1 = 10;
+  int num2 = 15;
+  int max = 0;
+
+  if (num1 > num2) {
+    max = num1;
+  } else {
+    max = num2;
+  }
+  print("The number is $max");
+}
